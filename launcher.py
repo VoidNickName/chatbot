0b82fa90e3cc487794d3a524f6c381ceeab135e1
Nick Admiraal <100027940+VoidNickName@users.noreply.github.com>
2026-06-27 01:34:13 +0800

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -12,8 +12,6 @@ window = None
 def run_server():
     if WEBVIEW:
         uvicorn.run(app, host="127.0.0.1", port=8000)
-    else:
-        uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
 def wait_for_server():
